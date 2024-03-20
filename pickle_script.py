d461067a16adcf598185b9c738c19a7d6823b1b2
nathaneckel <nathan@patientpaperwork.com>
2024-03-20 16:50:15 -0400

    initial commit

---
+++ pickle_script.py
@@ -0,0 +1,23 @@
+# FROM KRIS' LAB 10 - CONTEXTUALIZE THIS ONCE I DO THE OTHER STUFF.
+
+# if __name__ == '__main__':
+#
+#  with open('KrisBank.pickle', 'rb') as handle:
+#     zc_bank = pickle.load(handle)
+#
+#         runsomething()
+#
+#         with open('KrisBank.pickle', 'wb') as handle:
+#             pickle.dump(zc_bank, handle, protocol=pickle.HIGHEST_PROTOCOL)
+#
+#
+# zc_bank = Bank()
+# bob = Person("Bob", "Smith")
+# zc_bank.add_customer(bob)
+# #
+# with open('KrisBank.pickle', 'wb') as handle:
+#      pickle.dump(zc_bank, handle, protocol=pickle.HIGHEST_PROTOCOL)
+# #
+# with open('filename.pickle', 'rb') as handle:
+#     zc_bank2 = pickle.load(handle)
+#     print(zc_bank, zc_bank2)
