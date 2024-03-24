b05291587cc061131153fb2d67ccd111512331ba
nathaneckel <nathan@patientpaperwork.com>
2024-03-23 20:57:12 -0400

    tests aren't working otherwise done...

---
+++ data/json_helper_tests.py
@@ -0,0 +1,58 @@
+import json
+import os
+import unittest
+
+from json_helper import read_json, read_json_files_and_print_contents
+
+
+class TestFunctions(unittest.TestCase):
+
+    def test_read_json(self):
+        # Create a temporary JSON file with known data
+        temp_json_file = 'temp.json'
+        json_data = {'key': 'value'}
+        with open(temp_json_file, 'w') as f:
+            json.dump(json_data, f)
+
+        # Test read_json function
+        result = read_json(temp_json_file)
+        self.assertEqual(result, json_data)
+
+        # Clean up temporary file
+        os.remove(temp_json_file)
+
+    def test_read_json_files_and_print_contents(self):
+        # Create a temporary directory with known JSON files
+        temp_dir = 'temp_dir'
+        os.makedirs(temp_dir)
+        json_data = {'key': 'value'}
+        with open(os.path.join(temp_dir, 'file1.json'), 'w') as f:
+            json.dump(json_data, f)
+        with open(os.path.join(temp_dir, 'file2.json'), 'w') as f:
+            json.dump(json_data, f)
+
+        # Redirect stdout to capture print statements
+        import sys
+        from io import StringIO
+        saved_stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Test read_json_files_and_print_contents function
+        read_json_files_and_print_contents(temp_dir)
+
+        # Get printed contents
+        printed_contents = sys.stdout.getvalue()
+
+        # Assert that printed contents match expected
+        expected_contents = str(json_data) + '\n' * 2
+        self.assertEqual(printed_contents, expected_contents)
+
+        # Clean up temporary directory
+        os.remove(os.path.join(temp_dir, 'file1.json'))
+        os.remove(os.path.join(temp_dir, 'file2.json'))
+        os.rmdir(temp_dir)
+
+    # Add similar tests for write_pickle and load_pickle functions
+
+if __name__ == '__main__':
+    unittest.main()
--- json_helper.py
+++ json_helper.py
@@ -1,31 +1,41 @@
 import json
-def read_json():
-    with open('/Users/nathan/Dev/Labs/PythonFundamentals.Exercises.Part9/data/He-Man Action Figures/He-Man.json') as file:
-        return file.load(file)
-
-read_json()
+import os
+import pickle
 
 
-#OPEN > CONVERT > RETURN json object
-# #1. #given a string representing filePath:
-##this function must OPEN the json @filePath and
-# #convert contents to a json Object. #Json object should be returned
+def read_json(file):
+    with open(file, 'r') as file:
+        json_data = json.load(file)
+        return json_data
+
+
+file = 'data/He-Man_Figures/He-Man.json'
+json_object = read_json(file)
+print(json_object)
+
 
 # #Part B
-# Define a function called read_all_json_files.
-# Given a string representing a path to a directory,
-# - read all of the json files + return a list containing all json objects.
-# - incorporate the work from part A.
+# Given a string representing a path to a directory, read all of the json files + return a list containing all json objects.
 
-def read_all_json_files():
-    json_data = open('~/Dev/Labs/PythonFundamentals.Exercises.Part9')
+def read_json(file_path):
+    with open(file_path, 'r') as json_file:
+        json_data = json.load(json_file)
 
-    for item in json_data:
-        for data_item in item['data']:
-                print
-                data_item['name'], data_item['value']
+    return json_data
 
-read_all_json_files()
+
+
+def read_json_files_and_print_contents(dir_path):
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if file.endswith('.json'):
+                file_path = os.path.join(root, file)
+                json_data = read_json(file_path)
+                print(json_data)
+
+# Example usage:
+dir_path = 'data'
+read_json_files_and_print_contents(dir_path)
 
 
 # Part C
@@ -34,7 +44,43 @@ read_all_json_files()
 # the function should write the contents of the json files to a file
 # called super_smash_characters.pickle.
 
-# #Part D
-# Define a function called load_pickle. Given a file path, this function opens a pickled file and returns the data.
-# Use this function to print the pickled data from Part C to the screen.
-# Make sure to write tests for your code
+def write_pickle(json_file_path, pickle_file_path):
+    try:
+    #LOADING JSON data from JSON file
+        with open(json_file_path, 'r') as json_file:
+            json_data = json.load(json_file)
+
+        #SERIALIZE and SAVE JSON data with pickle
+        with open(pickle_file_path, 'wb') as pickle_file:
+            pickle.dump(json_data, pickle_file)
+        print("Pickle file created successfully.")
+    except FileNotFoundError:
+        print("JSON file was not found.")
+    except Exception as e:
+        print(f"An error occurred: (e)")
+
+
+json_file_path = '/Users/nathan/Dev/Labs/PythonFundamentals.Exercises.Part9/data/super_smash_bros/mario.json'
+pickle_file_path = 'data/super_smash_characters.pickle'
+
+
+write_pickle(json_file_path, pickle_file_path)
+
+
+# Define a function called load_pickle. Given a file path, this function opens a pickled file and returns the data.# Use this function to print the pickled data from Part C to the screen.# Make sure to write tests for your code
+
+def load_pickle(pickle_file_path):
+    try:
+        with open(pickle_file_path, 'rb') as pickle_file:
+            pickle_data = pickle.load(pickle_file)
+        return pickle_data
+    except FileNotFoundError:
+        print("Pickle file not found")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+pickle_file_path = 'data/super_smash_characters.pickle'
+pickle_data = load_pickle(pickle_file_path)
+print(load_pickle(pickle_file_path))
+
